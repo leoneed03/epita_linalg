f6fa145a82bc6d2fe1ca593503c5afa5fae380aa
l.senyukov <l.senyukov@tinkoff.ru>
2022-05-07 14:31:53 +0200

    check for det != 0 and raise an Exception

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import numpy as np
 
+
+class Error(Exception):
+    pass
+
+class WrongInputError(Error):
+    pass
+
 def get_exit_command():
     return 'escape'
+
 
 def check_if_need_to_exit(input):
     return input == get_exit_command()
 
+
 def print_welcome_message():
     print('Welcome to the change of basis app\nPrint ', get_exit_command(), ' to exit')
+
 
 def read_vector(N):
     print('Please input a vector of dimension N using \',\' as a delimeter', N)
@@ -17,12 +27,15 @@ def read_vector(N):
     vv = np.array([float(x) for x in values])
 
     if len(vv) != N:
-        raise ValueError('Expected vector of dimension N =', N, 'and actual N =', len(vv))
+        print('Expected vector of dimension N =', N, 'and actual N =', len(vv))
+        raise WrongInputError()
 
     return vv
 
+
 def read_matrix(N):
-    print('Please input a square matrix of dimension N', N, '\n line by line using \',\' as a delimeter')
+    print('Please input a square matrix of dimension N = ',
+          N, '\n line by line using \',\' as a delimeter representing N linearly independent vectors as a basis')
     m = np.eye(N)
 
     for i in range(N):
@@ -32,10 +45,16 @@ def read_matrix(N):
         v = np.array([float(x) for x in values])
 
         if len(v) != N:
-            raise ValueError('Expected vector of dimension N =', N, 'and actual N =', len(v))
+            print('Expected vector of dimension N =', N, 'and actual N =', len(v))
+            raise WrongInputError()
         m[i] = v
 
+    if abs(np.linalg.det(m)) < 1e-6:
+        print('Provided matrix is singular, it does not represent a basis')
+        raise WrongInputError()
+
     return m
+
 
 def change_basis():
     print('Please input N = number of dimensions')
@@ -44,16 +63,19 @@ def change_basis():
     v = read_vector(N)
     m = read_matrix(N)
 
-    print(v)
-    print(m)
-
     return np.linalg.inv(m).dot(v)
+
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_welcome_message()
 
-    result = change_basis()
-    print('End: ', result)
+    try:
+        print_welcome_message()
+
+        result = change_basis()
+        print('Vector in the new basis: ', result)
+    except WrongInputError:
+        print("Please provide correct input next time\nGoodbye!")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
