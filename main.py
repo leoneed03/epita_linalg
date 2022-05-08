20cf3a15017b5e052fa99f6d42a92e7834153f5f
l.senyukov <l.senyukov@tinkoff.ru>
2022-05-08 22:49:00 +0200

    add matplotlib for vector plotting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from matplotlib import pyplot as plt
 
 class Error(Exception):
     pass
@@ -7,31 +7,25 @@ class Error(Exception):
 class WrongInputError(Error):
     pass
 
-def get_exit_command():
-    return 'escape'
-
-def check_if_need_to_exit(input):
-    return input == get_exit_command()
-
 def print_welcome_message():
-    print('Welcome to the change of basis app\nPrint ', get_exit_command(), ' to exit')
+    print('Welcome to the change of basis app')
 
 def read_vector(N):
     print('Please input a vector of dimension N using \',\' as a delimeter', N)
     s = input()
     values = s.split(',')
 
-    vv = np.array([float(x) for x in values])
+    v = np.array([float(x) for x in values])
 
-    if len(vv) != N:
-        print('Expected vector of dimension N =', N, 'and actual N =', len(vv))
+    if len(v) != N:
+        print('Expected vector of dimension N =', N, 'and actual N =', len(v))
         raise WrongInputError()
 
-    return vv
+    return v
 
 def read_matrix(N):
     print('Please input a square matrix of dimension N = ',
-          N, '\n line by line using \',\' as a delimeter representing N linearly independent vectors as a basis')
+          N, '\n line by line using \',\' as a delimeter representing N linearly independent vectors as a basis column wise')
     m = np.eye(N)
 
     for i in range(N):
@@ -58,13 +52,31 @@ def change_basis():
     v = read_vector(N)
     m = read_matrix(N)
 
+    if N == 2:
+        print('Do you want to save a plot with your vector and basis? y/n')
+        answer = input()
+        if (answer == "y"):
+            print('Please provide a filename')
+            filename = input()
+
+            xs = np.append(np.array(m[0, :]), v[0])
+            ys = np.append(np.array(m[1, :]), v[1])
+            plt.quiver([0, 0, 0], [0, 0, 0], xs, ys, color=['b', 'b', 'g'], angles='xy', scale_units='xy', scale=1)
+
+            lim_x = max([max(abs(m[0, :])), abs(v[0])]) + 1
+            lim_y = max([max(abs(m[1, :])), abs(v[1])]) + 1
+            print(max(abs(m[0, :])))
+            print()
+            plt.xlim(-lim_x, lim_x)
+            plt.ylim(-lim_y, lim_y)
+
+            plt.savefig(filename)
+
     return np.linalg.inv(m).dot(v)
 
 if __name__ == '__main__':
-
     try:
         print_welcome_message()
-
         result = change_basis()
         print('Vector in the new basis: ', result)
     except WrongInputError:
